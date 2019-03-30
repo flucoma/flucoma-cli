43700cfd3fe58776c1fb11ee71bd1eb5aa1c1160
Pierre Alexandre Tremblay <info@pierrealexandretremblay.com>
2019-03-30 16:59:58 +0000

    removed the 'compose' CLI as it currently makes not sense to overwrite destinations

--- src/compose/compose.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <FluidCLIWrapper.hpp>
-#include <clients/nrt/BufferComposeNRT.hpp>
-
-int main(int argc, const char* argv[])
-{
-  using namespace fluid::client;
-  return CLIWrapper<BufferComposeClient>::run(argc, argv);
-}
